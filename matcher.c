107dc238367fde97f6ee55bbd28d653575c38cb6
mishthekeash <63427218+mishthekeash@users.noreply.github.com>
2022-12-28 12:44:38 -0800

    Add files via upload

---
+++ matcher.c
@@ -0,0 +1,42 @@
+/**
+ * You can modify this file.
+ */
+
+#include "matcher.h"
+
+/**
+ * Your helper functions can go below this line
+ */
+
+
+
+/**
+ * Your helper functions can go above this line
+ */
+
+
+/**
+ * Returns true if partial_line matches pattern, starting from
+ * the first char of partial_line.
+ */
+int matches_leading(char *partial_line, char *pattern) {
+  // You can use this recommended helper function, or not.
+
+  return 0;
+}
+
+/**
+ * Implementation of your matcher function, which
+ * will be called by the main program.
+ *
+ * You may assume that both line and pattern point
+ * to reasonably short, null-terminated strings.
+ */
+int rgrep_matches(char *line, char *pattern) {
+
+    //
+    // TODO put your code here.
+    //
+
+    return 0;
+}
